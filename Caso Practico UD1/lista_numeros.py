800f948276e63c6dc04e28a33ac946034a67ec23
rubengago <64603694+rubengago@users.noreply.github.com>
2023-03-30 21:39:08 +0200

    Mostrar valores de lista de 7 numeros aleatorios
    Programa que incluye una lista de 7 números al azar e imprime cada valor en pantalla.

---
+++ Caso Practico UD1/lista_numeros.py
@@ -0,0 +1,6 @@
+# Defino la lista
+lista_numeros = [1, 3.14, -689, 0.45, 7, 22.2, 51]
+
+# Bucle para recorrer la lista y mostrar los valores por pantalla
+for numero in lista_numeros:
+    print(numero)
