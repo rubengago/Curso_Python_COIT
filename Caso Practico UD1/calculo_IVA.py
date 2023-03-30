ee398a3235ba3299b3893ea205d97a81c521b65c
rubengago <64603694+rubengago@users.noreply.github.com>
2023-03-30 20:57:10 +0200

    Create calculo_IVA.py

---
+++ Caso Practico UD1/calculo_IVA.py
@@ -0,0 +1,28 @@
+# Rutina que comprueba si una cadena se corresponde con un valor float o no
+def isFloat(str):
+    try:
+        float(str)
+        return True
+    except ValueError:
+        return False
+
+# Solicito un valor al usuario
+valor_entrada = input("Introzuca el importe (para introducir un valor con decimales debe usar el punto como separador): ")
+
+''' 
+Reviso que el valor se corresponda con un float. En caso contrario, se muestra un mensaje de error
+'''
+if isFloat(valor_entrada):
+    importe = float(valor_entrada)
+    # Compruebo que el importe sea mayor que 0. Se entiende que el calculo no aplica para importes negativos
+    if importe > 0:
+        iva_importe = importe * IVA
+        total = importe + iva_importe
+        print("El IVA para un importe de", importe, "€ es de", iva_importe, "€")
+        print("El total (importe + IVA) corresponde a", total, "€")
+    else:
+        print("El importe introducido debe ser mayor que 0.")
+        exit(1)
+else:
+    print("El valor introducido no es correcto.")
+    exit(2)
